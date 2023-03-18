812a5c780f43fc7844128319574f8a6f37d1ca52
bernardo.amim <bernardo.amim@mercatus.com>
2023-03-18 16:11:29 -0300

    feat: creating isolated logger

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"github.com/bernardoamim/go-jobs/config"
 	"github.com/bernardoamim/go-jobs/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+	logger = *config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config init error: %v", err)
+		return
+	}
+
 	// Initialize router
 	router.Initialize()
 }
