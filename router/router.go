51045d8fa8289e1cf7c04a7b1f0dfa77fcb4abbb
bernardo.amim <bernardo.amim@mercatus.com>
2023-03-18 15:29:15 -0300

    creating crud for openings

--- router/router.go
+++ router/router.go
@@ -3,14 +3,9 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	// Initializing router with GIN default configs
 	router := gin.Default()
-	// Defining /ping route
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	initializeRoutes(router)
 
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
